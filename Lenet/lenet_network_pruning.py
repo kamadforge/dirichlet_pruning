029235ffe95a08a8287350239124eb05a60370b4
Kamil <kamil.m.adamczewski@gmail.com>
2021-02-11 18:05:22 +0100

    loading data and model from the external files, changing paths MAJOR

--- Lenet/lenet_network_pruning.py
+++ Lenet/lenet_network_pruning.py
@@ -5,38 +5,21 @@
 # -retrains the model
 # - computes the combinations with the pruned model
 
-
 #IMPORTANT
 #for 99.27 and 90.04 models remove output = self.out7(output)
 
 
-import torch
-from torch import nn, optim
 import sys
-
-
-
 import torch
-import torch.optim as optim
 from torch import nn, optim
-import torch.nn.functional as f
-
 import torch.utils.data
-from torchvision import datasets, transforms
-
 import numpy as np
-import csv
-import pdb
 import os
 import socket
-
-from sklearn.model_selection import ParameterGrid
-
 import argparse
 
-
 #############
-# params
+# PARAMS
 
 arguments=argparse.ArgumentParser()
 arguments.add_argument("--arch", default="7,10,40,20")
@@ -46,287 +29,114 @@ arguments.add_argument("--switch_samps", default=150, type=int)
 arguments.add_argument("--switch_comb", default='train') #train, load
 arguments.add_argument("--dataset", default="mnist")
 arguments.add_argument("--early_stopping", default=500, type=int)
+arguments.add_argument("--batch_size", default=105, type=int)
+arguments.add_argument("--trainval_perc", default=0.8, type=float)
 
 arguments.add_argument("--resume", default=False)
 arguments.add_argument("--prune_bool", default=False)
 arguments.add_argument("--retrain", default=False)
 
-arguments.add_argument("--path", default="None")
+arguments.add_argument("--path_checkpoint_load")
+arguments.add_argument("--path_checkpoint_save", default="checkpoint")
 
 args=arguments.parse_args()
-path=args.path
+print(args)
 
-#######
+evaluation="test"
+device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+dataset=args.dataset
+os.makedirs(args.path_checkpoint_save, exist_ok=True)
+path_checkpoint_save_scratch= os.path.join(args.path_checkpoint_save, "scratch", dataset)
+os.makedirs(path_checkpoint_save_scratch, exist_ok=True)
+path_checkpoint_save_retrain = os.path.join(args.path_checkpoint_save, "retrain", dataset)
+os.makedirs(path_checkpoint_save_retrain, exist_ok=True)
+
+######################### p
+
+################
 # path stuff
+
 cwd = os.getcwd()
 if 'g0' in socket.gethostname() or 'p0' in socket.gethostname():
     #the cwd is where the sub file is so ranking/
-    sys.path.append(os.path.join(cwd, "results_switch"))
+    sys.path.append(os.path.join(cwd, "results_switch_new"))
     path_compression = os.path.join(cwd, "results_compression")
     path_main= cwd
 else:
     #the cwd is results_compression
     parent_path = os.path.abspath('..')
-    sys.path.append(os.path.join(parent_path, "results_switch"))
+    sys.path.append(os.path.join(parent_path, "results_switch_new"))
     path_compression = cwd
     path_main= parent_path
 
-print(cwd)
-print(sys.path)
+print(f"Current working directory is: {cwd}")
 
+#import parent module
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
 
-######################
-
-
-from torch.nn.parameter import Parameter
-import magnitude_rank
+from methods import magnitude_rank
 #from lenet_network_pruning_withcombinations import compute_combinations_lenet
 #from lenet_network_pruning_withcombinations import get_data
-from lenet5_switch_integral import run_experiment as run_experiment_integral
-from lenet5_switch_pointest import run_experiment as run_experiment_pointest
-
-
-
-device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
-
-trainval_perc=0.8
-BATCH_SIZE = 105
-
-dataset=args.dataset
-evaluation="test"
-adversarial_dataset=False
+from methods.lenet5_switch_integral import run_experiment as run_experiment_integral
+from methods.lenet5_switch_pointest import run_experiment as run_experiment_pointest
+from importlib.machinery import SourceFileLoader
+dataset_mnist = SourceFileLoader("module_mnist", "../dataloaders/dataset_mnist.py").load_module()
+dataset_fashionmnist = SourceFileLoader("module_fashionmnist", "../dataloaders/dataset_fashionmnist.py").load_module()
+model_lenet5 = SourceFileLoader("module_lenet", "../models/lenet5.py").load_module()
+from module_fashionmnist import load_fashionmnist
+from module_mnist import load_mnist
+from module_lenet import Lenet
 
 ###################################################
 # DATA
 
-
 if dataset=="fashionmnist":
-
-    trainval_dataset=datasets.FashionMNIST('data/FashionMNIST', train=True, download=True,
-                        #transform=transforms.Compose([transforms.ToTensor(),
-                        #transforms.Normalize((0.1307,), (0.3081,))]),
-                        transform=transforms.ToTensor())
-
-    train_size = int(trainval_perc * len(trainval_dataset))
-    val_size = len(trainval_dataset) - train_size
-    torch.manual_seed(0)
-    train_dataset, val_dataset = torch.utils.data.random_split(trainval_dataset, [train_size, val_size])
-
-    test_dataset=datasets.FashionMNIST('data/FashionMNIST', train=False, transform=transforms.ToTensor())
-
-    if adversarial_dataset:
-        tensor_x = torch.load('../results_adversarial/data/FashionMNIST_adversarial/tensor_x.pt')
-        tensor_y = torch.load('../results_adversarial/data/FashionMNIST_adversarial/tensor_y.pt')
-        tensor_x = tensor_x.unsqueeze(1)
-        tensor_y=tensor_y.squeeze(1)
-
-        test_dataset = torch.utils.data.TensorDataset(tensor_x, tensor_y)  # create your datset
-
-    print("Loading,", dataset)
-
+    train_loader, test_loader, val_loader = load_fashionmnist(args.batch_size, args.trainval_perc)
 elif dataset=="mnist":
+    train_loader, test_loader, val_loader = load_mnist(args.batch_size, args.trainval_perc)
 
-    trainval_dataset = datasets.MNIST('data/MNIST', train=True, download=True,
-                                             # transform=transforms.Compose([transforms.ToTensor(),
-                                             # transforms.Normalize((0.1307,), (0.3081,))]),
-                                             transform=transforms.ToTensor())
-
-    train_size = int(trainval_perc * len(trainval_dataset))
-    val_size = len(trainval_dataset) - train_size
-    torch.manual_seed(0)
-    train_dataset, val_dataset = torch.utils.data.random_split(trainval_dataset, [train_size, val_size])
-
-    test_dataset = datasets.MNIST('data/MNIST', train=False, transform=transforms.ToTensor())
-
-    print("Loading,", dataset)
-
-
-# Load datasets
-
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-    #datasets.MNIST('data', train=False, transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])),
-    test_dataset,
-    batch_size=BATCH_SIZE, shuffle=False)
-
-
-##############################################################################################3####33
-# ###############################################################################3##########
+###################################################################
 # NETWORK (conv-conv-fc-fc)
 
-class Identity(nn.Module):
-    def __init__(self):
-        super(Identity, self).__init__()
-
-    def forward(self, x):
-        return x
-
-class Lenet(nn.Module):
-    def __init__(self, nodesNum1, nodesNum2, nodesFc1, nodesFc2):
-        super(Lenet, self).__init__()
-
-        self.nodesNum2=nodesNum2
-
-        self.c1 = nn.Conv2d(1, nodesNum1, 5)
-        self.s2 = nn.MaxPool2d(2)
-        self.bn1 = nn.BatchNorm2d(nodesNum1)
-        self.c3 = nn.Conv2d(nodesNum1, nodesNum2, 5)
-        self.s4 = nn.MaxPool2d(2)
-        self.bn2 = nn.BatchNorm2d(nodesNum2)
-        self.c5 = nn.Linear(nodesNum2 * 4 * 4, nodesFc1)
-        self.f6 = nn.Linear(nodesFc1, nodesFc2)
-        self.out7 = nn.Linear(nodesFc2, 10)
-
-        #self.parameter = Parameter(-1e-10*torch.ones(nodesNum1),requires_grad=True) # this parameter lies #S
-
-        # Fisher method is called on backward passes
-        self.running_fisher = [0] * 4
-
-        self.act1=0
-        self.activation1 = Identity()
-        self.activation1.register_backward_hook(self._fisher1)
-
-        self.act2=0
-        self.activation2 = Identity()
-        self.activation2.register_backward_hook(self._fisher2)
-
-        self.act3=0
-        self.activation3 = Identity()
-        self.activation3.register_backward_hook(self._fisher3)
-
-        self.act4=0
-        self.activation4 = Identity()
-        self.activation4.register_backward_hook(self._fisher4)
-
-
-
-
-
-    def forward(self, x):
-
-        # output=f.relu(self.fc1(x))
-        # output=self.bn1(output)
-        # output=f.relu(self.fc2(output))
-        # output=self.bn2(output)
-        # output=self.fc3(output)
-        # return output
-
-        # #x=x.view(-1,784)
-        output = self.c1(x)
-        output = f.relu(self.s2(output))
-        out = self.activation1(output)
-        self.act1 = out
-        output = self.bn1(output)
-
-        output = self.c3(output)
-        output = f.relu(self.s4(output))
-        out = self.activation2(output)
-        self.act2 = out
-        output = self.bn2(output)
-
-        output = output.view(-1, self.nodesNum2 * 4 * 4)
-        output = self.c5(output)
-        out = self.activation3(output)
-        self.act3 = out
-
-
-        output = self.f6(output)
-        out = self.activation4(output)
-        self.act4 = out
-        output = self.out7(output) #remove for 99.27 and 90.04 models
-
-        return output
-
-    def _fisher1(self, notused1, notused2, grad_output):
-        act1 = self.act1.detach()
-        grad = grad_output[0].detach()
-        #print(grad_output[0].shape)
-
-        g_nk = (act1 * grad).sum(-1).sum(-1)
-        del_k = g_nk.pow(2).mean(0).mul(0.5)
-        #print(del_k.shape)
-        self.running_fisher[0] += del_k
-
-    def _fisher2(self, notused1, notused2, grad_output):
-        act2 = self.act2.detach()
-        grad = grad_output[0].detach()
-
-        g_nk = (act2 * grad).sum(-1).sum(-1)
-        del_k = g_nk.pow(2).mean(0).mul(0.5)
-        self.running_fisher[1] += del_k
-
-    def _fisher3(self, notused1, notused2, grad_output):
-        act3 = self.act3.detach()
-        grad = grad_output[0].detach()
-
-        g_nk = (act3 * grad)
-        del_k = g_nk.pow(2).mean(0).mul(0.5)
-        self.running_fisher[2] += del_k
-
-    def _fisher4(self, notused1, notused2, grad_output):
-        act4 = self.act4.detach()
-        grad = grad_output[0].detach()
-
-        g_nk = (act4 * grad)
-        del_k = g_nk.pow(2).mean(0).mul(0.5)
-        self.running_fisher[3] += del_k
-
-    def reset_fisher(self):
-        self.running_fisher[3] = 0 * self.running_fisher
-
-    def cost(self):
-
-        in_channels = self.in_channels
-        out_channels = self.out_channels
-        middle_channels = int(self.mask.sum().item())
-
-        conv1_size = self.conv1.weight.size()
-        conv2_size = self.conv2.weight.size()
-
-        # convs
-        self.params = in_channels * middle_channels * conv1_size[2] * conv1_size[
-            3] + middle_channels * out_channels * \
-                      conv2_size[2] * conv2_size[3]
-
-        # batchnorms, assuming running stats are absorbed
-        self.params += 2 * in_channels + 2 * middle_channels
-
-        # skip
-        if not self.equalInOut:
-            self.params += in_channels * out_channels
-        else:
-            self.params += 0
-
-
-
-##################################
-
-
+device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 nodesNum1, nodesNum2, nodesFc1, nodesFc2=10,20,100,25
 net=Lenet(nodesNum1,nodesNum2,nodesFc1,nodesFc2).to(device)
 criterion = nn.CrossEntropyLoss()
-
 #optimizer=optim.Adam(net.parameters(), lr=0.001)
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
-
 
 ######################################
 #LOADING MODEL/RESUME
 
-def load_model():
-    global path
-    path=args.path
-    if path=="None":
-        print("Please provide checkpoint path")
-        sys.exit()
-    else:
-        net.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage)['model_state_dict'], strict=False)
+def load_model(path_checkpoint_load):
 
-    print(dataset, "loaded.")
-    print(os.path.split(path)[1])
-    return net
+    print("Loading from... ", path_checkpoint_load)
+    if path_checkpoint_load is None:
+        print("The checkpoint not indicated, looking for the existing checkpoints.")
+        checkpoints = os.listdir(path_checkpoint_save_scratch)
+        # if there are no checkpoints from previous training
+        if len(checkpoints)==0:
+            print("No checkpoints found.")
+            sys.exit()
+        else:
+            # take the checkpoint with the highest accoracy
+            checkpoint_accuracies = []
+            for element in checkpoints:
+                checkpoint_accuracies.append(float(element[-5:]))
+            argsorted = np.argsort(checkpoint_accuracies)
+            path_checkpoint_load = os.path.join(path_checkpoint_save_scratch, checkpoints[argsorted[-1]])
+
+    net.load_state_dict(torch.load(path_checkpoint_load, map_location=lambda storage, loc: storage)['model_state_dict'], strict=False)
+
+    print("\nModel loaded:")
+    print(os.path.split(path_checkpoint_load)[1])
+    return net, path_checkpoint_load
 
 
 
@@ -695,8 +505,8 @@ comp_combinations=False
 
 
 if resume:
-    load_model()
-    evaluate()
+    net, path_checkpoint_load_ret = load_model(args.path_checkpoint_load)
+    acc = evaluate()
     #if comp_combinations:
     #    compute_combinations_lenet(False, net, evaluate, dataset, "zeroing") #can be "additive noise instead of zeroing
 
